507a61b9440b76e39fc09001dbcb06250242e5c5
kyopread <djflsthskan2@gmail.com>
2022-10-22 01:14:47 +0900

    feat: 코틀린 19강 이모저모

---
+++ src/main/kotlin/com/ktstudy/lec19/Lec19Main.kt
@@ -0,0 +1,10 @@
+package com.ktstudy.lec19
+
+// 타입에 대한 별칭을 줄 수 있는 typealias 키워드
+// Import 당시 이름을 바꿀 수 있는 as import 기능 존재
+// 변수를 한번에 선언할 수 있는 구조분해 기능이 있고, componentN 함수를 사용한다.
+// (name, age) = person
+// component1, component2
+// for while문과 달리 forEach에는 break와 continue를 사용할 수 없다.
+// takeIf, takeUnless를 활용해 코드 양을 줄이고
+// method chaning을 활용할 수 있다.
